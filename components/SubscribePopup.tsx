5cffe5c232e9c40906e0da503799fcb4e6673bfd
nrimsky <ninarimsky@gmail.com>
2023-02-26 20:18:27 +0000

    Stop propogration

--- components/SubscribePopup.tsx
+++ components/SubscribePopup.tsx
@@ -1,6 +1,9 @@
-export default function SubscribePopup(props: {onDismiss: () => void}) {
+export default function SubscribePopup(props: { onDismiss: () => void }) {
   return (
-    <div className="fixed top-0 bottom-0 w-screen backdrop-blur" onClick={props.onDismiss}>
+    <div
+      className="fixed top-0 bottom-0 w-screen backdrop-blur"
+      onClick={props.onDismiss}
+    >
       <div className="relative w-screen p-4 md:p-8">
         <form
           className="flex flex-col gap-4 bg-blue-100 p-20 rounded shadow-xl max-w-sm mx-auto border border-primary"
@@ -8,6 +11,9 @@ export default function SubscribePopup(props: {onDismiss: () => void}) {
           method="POST"
           data-netlify="true"
           action="/success"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
         >
           <label className="font-medium text-primary" htmlFor="email">
             Subscribe to our newsletter
@@ -20,7 +26,10 @@ export default function SubscribePopup(props: {onDismiss: () => void}) {
             placeholder="Enter email here"
             className="py-1 px-2 font-light text-black outline-none text-sm"
           />
-          <button className="bg-primary text-white font-medium rounded-sm py-1 px-4 text-sm hover:bg-blue-400">
+          <button
+            className="bg-primary text-white font-medium rounded-sm py-1 px-4 text-sm hover:bg-blue-400"
+            type="submit"
+          >
             Subscribe
           </button>
         </form>
