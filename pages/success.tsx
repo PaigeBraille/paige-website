298eb78da8835a90d2eb8ada195455e0dc4bca86
nrimsky <ninarimsky@gmail.com>
2023-02-26 20:10:08 +0000

    Remove '

--- pages/success.tsx
+++ pages/success.tsx
@@ -4,7 +4,7 @@ export default function Success() {
   return (
     <div className="flex flex-col w-full h-full justify-center items-center gap-4 pb-32 pt-16 text-center">
       <h1 className="text-xl font-light leading-tight px-4">
-        Thank you for subscribing to Paige's newsletter
+        Thank you for subscribing to the Paige newsletter
       </h1>
       <Link href="/">
         <button className="font-bold text-white bg-primary px-8 py-2 text-sm rounded cursor-pointer">
