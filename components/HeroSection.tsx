9580c1d2c72e326738a5c7fad0c259e065db9142
nrimsky <ninarimsky@gmail.com>
2023-04-15 19:51:38 +0100

    Add video

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,23 +1,32 @@
-import Image from "next/image";
-import HeroImage from "../public/hero_connect.png";
 import Link from "next/link";
 
 export default function HeroSection() {
   return (
     <div className="flex flex-col gap-4 w-full xl:flex-row">
-      <div className="flex min-w-1/2">
-        <Image
-          src={HeroImage}
-          alt="Hero Image"
-          className="max-h-96 object-contain overflow-clip"
-        />
+      <div className="flex xl:max-w-1/3">
+        <video
+          src={"hero-video.mp4"}
+          width="auto"
+          height="400"
+          autoPlay
+          muted
+          loop
+          controls={false}
+          preload="auto"
+          poster={"hero_connect.png"}
+          className="shadow-none"
+        >
+          Video of paige connect showing the braille writer and the print
+          translation in the accompanying app
+        </video>
       </div>
-      <div className="flex flex-col gap-4 text-center xl:w-3/5 xl:text-start p-4 xl:pr-10 xl:pt-32">
+      <div className="flex flex-col gap-4 text-center xl:text-start p-4 xl:pr-10 xl:pt-32">
         <h1 className="leading-tight text-xl font-bold tracking-tight font-extrabold">
           What is Paige Connect?
         </h1>
         <p className="text-gray-600">
-          An adaptation for braille writers that lets you share the print translation.
+          An adaptation for braille writers that lets you share the print
+          translation.
         </p>
         <Link
           href="/products"
