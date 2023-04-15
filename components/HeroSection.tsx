3ec7256d5cb09347883231ff2c5e21a7f77e2a30
gos2two <sergio.gosalvez@imperial.ac.uk>
2023-04-15 16:07:55 +0100

    HeroImage change

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import HeroImage from "../public/home_connect.png";
+import HeroImage from "../public/hero_connect.png";
 import Link from "next/link";
 
 export default function HeroSection() {
@@ -12,7 +12,7 @@ export default function HeroSection() {
           className="max-h-96 object-contain overflow-clip"
         />
       </div>
-      <div className="flex flex-col gap-4 text-center xl:w-3/5 xl:text-start p-4 xl:pr-10 xl:pt-20">
+      <div className="flex flex-col gap-4 text-center xl:w-3/5 xl:text-start p-4 xl:pr-10 xl:pt-32">
         <h1 className="leading-tight text-xl font-bold tracking-tight font-extrabold">
           What is Paige Connect?
         </h1>
