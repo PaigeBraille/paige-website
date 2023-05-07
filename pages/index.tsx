42eebb6ead4a201b8279459673d79e7d2aa8fbfc
nrimsky <ninarimsky@gmail.com>
2023-05-07 17:30:38 +0100

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import {
   GraphicSection1,
   GraphicSection2,
-  GraphicSection3,
 } from "../components/GraphicSections";
 import Partners from "../components/Partners";
 import TestimonialsSection from "../components/Testimonials";
@@ -15,7 +14,6 @@ export default function Home() {
     <Wrapper>
       <div className="max-w-5xl mx-auto flex flex-col gap-12 sm:gap-16 sm:px-6 py-6 overflow-clip">
         <HeroSection />
-        {/* <GraphicSection3 /> */}
         <GraphicSection1 />
         <TestimonialsSection />
         <GraphicSection2 />
