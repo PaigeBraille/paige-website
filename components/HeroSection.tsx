d420b60ba35039c0264e4ef774e7140ce591380e
nrimsky <ninarimsky@gmail.com>
2023-05-07 17:23:36 +0100

    Change to gif

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,26 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
+import HeroVideo from "../public/hero-video.gif";
 
 export default function HeroSection() {
   return (
-    <div className="flex flex-col gap-4 w-full xl:flex-row">
-      <div className="flex xl:max-w-1/3">
-        <video
-          src={"hero-video.mp4"}
-          width="auto"
+    <div className="flex flex-col gap-4 w-full lg:flex-row">
+      <div className="flex lg:max-w-1/3 justify-center">
+        <Image
+          src={HeroVideo}
+          alt="Animation of paige connect showing the braille writer and the print translation in the accompanying app"
           height="400"
-          autoPlay
-          muted
-          loop
-          controls={false}
-          preload="auto"
-          className="shadow-none"
-        >
-          Video of paige connect showing the braille writer and the print
-          translation in the accompanying app
-        </video>
+          className="object-contain"
+          priority={true}
+        />
       </div>
-      <div className="flex flex-col gap-4 text-center xl:text-start p-4 xl:pr-10 xl:pt-32">
-        <h1 className="leading-tight text-xl font-bold tracking-tight font-extrabold">
+      <div className="flex flex-col gap-4 text-center lg:text-start p-4 lg:pr-10 lg:pt-32">
+        <h1 className="leading-tight text-lg font-bold tracking-tight font-extrabold">
           What is Paige Connect?
         </h1>
         <p className="text-gray-600">
@@ -29,7 +24,7 @@ export default function HeroSection() {
         </p>
         <Link
           href="/products"
-          className="bg-primary text-white font-medium rounded-sm py-1 px-4 inline-flex mx-auto xl:mr-auto xl:ml-0 hover:bg-blue-900"
+          className="bg-primary text-white font-medium rounded-sm py-1 px-4 inline-flex mx-auto lg:mr-auto lg:ml-0 hover:bg-blue-900"
         >
           Find out more
         </Link>
