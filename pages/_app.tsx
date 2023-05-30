59643873d644fee1057f73f76d6a4939073347be
gos2two <sergio.gosalvez@imperial.ac.uk>
2023-05-30 15:03:00 +0100

    Remove ReactGA library.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import ReactGA from 'react-ga';
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-
-ReactGA.initialize('G-01J74RK5B9');
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   
