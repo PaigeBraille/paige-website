2c321e3e1a47d87b4c5dcf822f79c1e363ec1e60
GH013 <73078514+GH013@users.noreply.github.com>
2023-07-16 00:39:38 +0100

    Update ManualSection.tsx
    Repeated word

--- components/ManualSection.tsx
+++ components/ManualSection.tsx
@@ -6,7 +6,7 @@ export default function ManualSection() {
     <div className="flex flex-col md:flex-row px-4 pt-4 md:p-8 my-4 sm:my-8 justify-between relative overflow-visible gap-4 xl:rounded-lg">
       <div className="flex flex-col justify-between md:w-1/2 gap-4">
         <h2 className="font-bold tracking-tight leading-tight text-xl md:text-2xl">
-          You can also download the the Paige Connect manual in PDF format
+          You can also download the Paige Connect manual in PDF format
         </h2>
         <a
           className="border rounded-sm bg-primary font-bold text-center text-white px-4 py-2 w-full sm:w-fit"
