ac6e1c8c2ab8f75bb5f1f0805b0d60b961177a21
GH013 <73078514+GH013@users.noreply.github.com>
2023-09-06 21:46:43 +0100

    Update buy.tsx
    Update video

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -81,7 +81,7 @@ const HeroSection = (props: { onClickJoin: () => void }) => {
         >
           <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/vf-0M0ug554?controls=0&rel=0&showinfo=0&loop=1&autoplay=1"
+            src="https://www.youtube.com/embed/AUHUtJbe6Ws?controls=0&rel=0&showinfo=0&loop=1&autoplay=1"
             title="Paige Connect in action translating embossep braille."
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
