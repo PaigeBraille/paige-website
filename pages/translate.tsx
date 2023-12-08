884ae929f9194171d450a22973d88d33b656941c
gos2two <sergio.gosalvez@imperial.ac.uk>
2023-12-08 15:53:09 +0100

    Fix dropdown size | Translate.tsx (2)

--- pages/translate.tsx
+++ pages/translate.tsx
@@ -198,12 +198,11 @@ export default function Translate() {
             <option value="es-g2.ctb">Spanish (Grade 2)</option>
             <option value="sv-g1.ctb">Swedish (Grade 1)</option>
             <option value="sv-g2.ctb">Swedish (Grade 2)</option>
-            <option value="zhcn-cbs.ctb" style={{ overflowWrap: "break-word" }}>Chinese common braille (simplified Chinese characters)</option>
-            <option value="zh-chn.ctb" style={{ overflowWrap: "break-word" }}>Chinese (China, Mandarin) Current Braille System (no tones)</option>
-            <option value="zhcn-g1.ctb" style={{ overflowWrap: "break-word" }}>Chinese (China, Mandarin) Current Braille System</option>
-            <option value="zhcn-g2.ctb" style={{ overflowWrap: "break-word" }}>Chinese (China, Mandarin) Double-phonic Braille System</option>
-            <option value="zh-hk.ctb" style={{ overflowWrap: "break-word" }}>Chinese (Hong Kong, Cantonese)</option>
-            <option value="zh-tw.ctb" style={{ overflowWrap: "break-word" }}>Chinese (Taiwan, Mandarin)</option>
+            <option className="break-all" value="zhcn-cbs.ctb" >Chinese common braille (simplified Chinese characters)</option>
+            <option className="break-all" value="zh-chn.ctb" >Chinese (China, Mandarin) Current Braille System (no tones)</option>
+            <option className="break-all" value="zhcn-g1.ctb" >Chinese (China, Mandarin) Current Braille System</option>
+            <option className="break-all" value="zhcn-g2.ctb" >Chinese (China, Mandarin) Double-phonic Braille System</option>
+            <option className="break-all" value="zh-hk.ctb" >Chinese (Hong Kong, Cantonese)</option>
           </select>
       </div>
         <div className="flex flex-col md:flex-row justify-between py-4 md:py-6">
