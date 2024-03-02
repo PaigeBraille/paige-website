519ea39f54abf630826169126b1d967b7a217235
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2024-03-02 15:09:00 +0000

    feat: Finish styling for sign-up page

--- pages/auth/sign-up.tsx
+++ pages/auth/sign-up.tsx
@@ -5,6 +5,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { auth } from "@/lib/firebase";
 import { Wrapper } from "@/components/Wrapper";
+import Link from "next/link";
 
 const SignUp = () => {
   const router = useRouter();
@@ -44,98 +45,99 @@ const SignUp = () => {
 
   return (
     <Wrapper>
-      <div className="max-w-5xl mx-auto md:px-6 flex flex-col">
-        <div className="px-6 md:px-0">
-          <div className="flex flex-col md:flex-row bg-blue-50 px-6 p-6 md:p-6 mt-6 justify-between relative overflow-visible gap-4 rounded-lg">
-            <div className="flex flex-col gap-2 md:w-3/5">
-              <h2 className="text-l font-bold text-primary tracking-tight">
-                Featured
-              </h2>
-              <div className="text-center">
-                <div className="flex justify-center">
-                  <Formik
-                    initialValues={initialValues}
-                    validationSchema={validationSchema}
-                    onSubmit={onSubmit}
-                  >
-                    <Form className="w-1/2">
-                      {error && <div className="text-red-500">{error}</div>}
-                      <div className="mb-4">
-                        <label htmlFor="signUpEmail" className="block">
-                          Email
-                        </label>
-                        <Field
-                          type="email"
-                          name="email"
-                          id="signUpEmail"
-                          placeholder="Email"
-                          className="border border-gray-300 rounded px-2 py-1 w-full"
-                        />
-                        <ErrorMessage
-                          name="email"
-                          component="div"
-                          className="text-red-500"
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label htmlFor="signUpPassword" className="block">
-                          Password
-                        </label>
-                        <Field
-                          type="password"
-                          name="passwordOne"
-                          id="signUpPassword"
-                          placeholder="Password"
-                          className="border border-gray-300 rounded px-2 py-1 w-full"
-                        />
-                        <ErrorMessage
-                          name="passwordOne"
-                          component="div"
-                          className="text-red-500"
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label htmlFor="signUpPassword2" className="block">
-                          Confirm Password
-                        </label>
-                        <Field
-                          type="password"
-                          name="passwordTwo"
-                          id="signUpPassword2"
-                          placeholder="Password"
-                          className="border border-gray-300 rounded px-2 py-1 w-full"
-                        />
-                        <ErrorMessage
-                          name="passwordTwo"
-                          component="div"
-                          className="text-red-500"
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <button
-                          type="submit"
-                          className="bg-blue-500 text-white px-4 py-2 rounded"
-                        >
-                          Sign Up
-                        </button>
-                      </div>
-                    </Form>
-                  </Formik>
-                </div>
-              </div>
-              <h2 className="text-3xl font-bold tracking-tight">Seren Jaye</h2>
-              <p className="text-sm mb-auto">
-                &ldquo;I am such a nerd for new accessible technology and this
-                was so cool!&rdquo;
-              </p>
-              <button
-                className="text-xs border border-black rounded px-4 py-2 mr-auto"
-                role="button"
-              >
-                Read full story
-              </button>
-            </div>
+      <div className="max-w-sm mx-auto pb-6">
+        <div className="bg-blue-50 px-6 p-6 rounded-lg">
+          <div className="pb-4">
+            <h2 className="text-l font-bold text-primary tracking-tight">
+              Sign up
+            </h2>
+            <p className="text-sm text-gray-600">
+              Sign up to store your progress through the learn tab
+            </p>
           </div>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            <Form>
+              {error && <div className="text-red-500 mb-4">{error}</div>}
+              <div className="mb-5">
+                <label
+                  htmlFor="signUpEmail"
+                  className="block text-xs text-gray-600"
+                >
+                  Email
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  id="signUpEmail"
+                  placeholder="Email"
+                  className="border border-gray-300 rounded px-2 py-1 w-full"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-xs text-red-700 absolute"
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="signUpPassword"
+                  className="block text-xs text-gray-600"
+                >
+                  Password
+                </label>
+                <Field
+                  type="password"
+                  name="passwordOne"
+                  id="signUpPassword"
+                  placeholder="Password"
+                  className="border border-gray-300 rounded px-2 py-1 w-full"
+                />
+                <ErrorMessage
+                  name="passwordOne"
+                  component="div"
+                  className="text-xs text-red-700 absolute"
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="signUpPassword2"
+                  className="block text-xs text-gray-600"
+                >
+                  Confirm Password
+                </label>
+                <Field
+                  type="password"
+                  name="passwordTwo"
+                  id="signUpPassword2"
+                  placeholder="Confirm Password"
+                  className="border border-gray-300 rounded px-2 py-1 w-full"
+                />
+                <ErrorMessage
+                  name="passwordTwo"
+                  component="div"
+                  className="text-xs text-red-700 absolute"
+                />
+              </div>
+              <div className="mt-8">
+                <button
+                  className="text-xs border border-black rounded px-4 py-2 mr-auto w-full"
+                  type="submit"
+                >
+                  Sign up
+                </button>
+              </div>
+            </Form>
+          </Formik>
+          <div className="border-t border-gray-900 mt-4 mb-1"></div>
+          <Link href="/auth/login">
+            <p className="text-gray-700 hover:underline text-xs hover:text-gray-500">
+              Already have an account?
+            </p>
+          </Link>
         </div>
       </div>
     </Wrapper>
